a2d0b94fc559c444c69519330d9e3048ffdd4588
rigolinux <rigoberto7567@gmail.com>
2023-09-24 09:33:16 -0600

    project setup with pinia

---
+++ src/counter/pages/Counter1Page.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Counter1Page</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'counter1',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/counter/pages/Counter1Page.vue')
+    }
+  ]
+})
+
+export default router
