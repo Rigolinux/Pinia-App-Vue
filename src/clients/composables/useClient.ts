0ac8a3d87575e3bb4ed27d667dccc371c6693abf
rigolinux <rigoberto7567@gmail.com>
2023-09-26 13:17:30 -0600

    create composable by user specific id

---
+++ src/clients/composables/useClient.ts
@@ -0,0 +1,35 @@
+import { ref, watch } from 'vue';
+import type { Client } from '../interfaces/Client';
+import { useQuery } from '@tanstack/vue-query';
+import clientsAPi from '@/api/clients-api';
+
+const getClient = async(id:number):Promise<Client> => {
+    const {data} = await clientsAPi.get(`/clients/${id}`);
+
+    return data;
+};
+
+
+const useClient = (id:number) =>{
+
+    const client = ref<Client>();
+
+    const {isLoading, data,} = useQuery(
+        ["client",id],
+        ()=>getClient(id)
+    )
+
+    watch(data,() => {
+        if(data.value){
+            client.value = data.value;
+        }
+    }, {immediate: true})
+
+    return {
+        isLoading,
+        client,
+
+    }
+};
+
+export default useClient;
--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -1,13 +1,48 @@
 <script setup lang="ts">
+import LoadingModal from '@/shared/components/LoadingModal.vue';
+import { useRoute } from 'vue-router';
+import useClient from '../composables/useClient';
+
+const route = useRoute();
+
+const { isLoading, client } = useClient(+route.params.id);
 </script>
 
 <template>
 
-    <div>
-        <h1>Clients by id</h1>
-    </div>
+  <h3>Guardando ...</h3>
+  <h3>Guardado</h3>
+
+  <LoadingModal v-if="isLoading" />
+
+  <div>
+    <h1>Client Page</h1>
+    <form>
+
+        <input type="text" placeholder="Name" />
+        <br />
+        <input type="text" placeholder="Adress" />
+        <br />
+        <button>Save</button>
+    </form>
+    <code>
+        information about client
+        {{client}}
+    </code>
+  </div>
 
 </template>
 
 <style scoped>
+
+input {
+    width: 100%;
+    padding: 5px 10px;
+    margin-bottom: 10px;
+}
+
+button{
+    margin-bottom: 10px;
+}
+
 </style>
